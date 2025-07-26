3ce64764b1a717cf66fc024e1ed7ac43e7bb0fb9
Sahil Patel <ssahillppatell@gmail.com>
2025-07-26 22:36:38 +0530

    Logo added

--- manifest.config.ts
+++ manifest.config.ts
@@ -7,6 +7,12 @@ export default {
     version: packageJson.version,
     description: packageJson.description,
     manifest_version: 3,
+    icons: {
+        "16": "icon16.png",
+        "32": "icon32.png",
+        "48": "icon48.png",
+        "128": "icon128.png"
+    },
     permissions: [
         "activeTab",
         "scripting"
@@ -22,9 +28,9 @@ export default {
             run_at: "document_end"
         }
     ],
-    action: {
-        default_popup: "src/ui/action-popup/index.html"
-    },
+    // action: {
+    //     default_popup: "src/ui/action-popup/index.html"
+    // },
     content_security_policy: {
         extension_pages: "script-src 'self' http://localhost:5173; worker-src 'self' http://localhost:5173; connect-src 'self' ws://localhost:5173 http://localhost:5173; object-src 'self'; style-src 'self' 'unsafe-inline';"
     },
