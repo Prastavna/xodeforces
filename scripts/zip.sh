d4259539de77699303329df13cc6e2f6c7542a25
Sahil Patel <ssahillppatell@gmail.com>
2025-08-28 22:42:48 +0530

    Removed ts.worker for Firefox release

--- scripts/zip.sh
+++ scripts/zip.sh
@@ -4,6 +4,11 @@
 VERSION=$(cat package.json | grep version | cut -d '"' -f 4)
 
 cd dist/firefox
+# remove ts.worker and create fake ts.worker
+rm ./ts.worker.worker.js
+echo "self.onmessage = function (_e) {
+	self.postMessage({ data: null });
+};" > ./ts.worker.worker.js
 zip -r ../../xodeforces-${VERSION}-firefox.zip .
 
 cd ../chrome
